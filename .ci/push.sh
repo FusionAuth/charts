9dc5096a8dad028437c90b0dad57a4e77c3d1140
Dennis Fricke <dennis.fricke@ninjaneers.de>
2020-02-03 18:04:49 +0100

    chore(travis): add ci yaml

---
+++ .ci/push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "ci@fusionauth.io"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout master
+  git add . *.yaml
+  git commit --message "Update index [skip ci]"
+}
+
+upload_files() {
+  git remote add remote https://${GITHUB_TOKEN}@github.com/FusionAuth/charts.git > /dev/null 2>&1
+  git push --quiet remote master
+}
+
+setup_git
+commit_website_files
+upload_files
